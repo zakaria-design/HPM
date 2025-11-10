6ae9a697e3bd166e4186d62fc2630fb77530d42c
zakaria <zakariaalfatih83@gmail.com>
2025-11-11 00:34:05 +0700

    update fitur absensi dan perbaikan bug

--- resources/views/karyawan/layouts/script.blade.php
+++ resources/views/karyawan/layouts/script.blade.php
@@ -1,6 +1,11 @@
-<script data-navigate-once src="{{ asset('adminlte3/plugins/jquery/jquery.min.js') }}"></script>
+{{-- <script data-navigate-once src="{{ asset('adminlte3/plugins/jquery/jquery.min.js') }}"></script>
 <script data-navigate-once src="{{ asset('adminlte3/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script data-navigate-once src="{{ asset('adminlte3/dist/js/adminlte.min.js') }}"></script>
+<script data-navigate-once src="{{ asset('adminlte3/dist/js/adminlte.min.js') }}"></script> --}}
+
+<!-- CSS AdminLTE -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
+<!-- JS AdminLTE -->
+<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
 
 {{-- sweet alert 2 --}}
 <script src="{{ asset('sweetalert2/dist/sweetalert2.all.min.js') }}"></script>
