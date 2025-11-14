8c5b5451337831054b898f4e75301af6256914bd
zakaria <zakariaalfatih83@gmail.com>
2025-11-15 00:04:17 +0700

    update project

--- resources/views/manager/layouts/script.blade.php
+++ resources/views/manager/layouts/script.blade.php
@@ -1,6 +1,3 @@
-{{-- <script data-navigate-once src="{{ asset('adminlte3/plugins/jquery/jquery.min.js') }}"></script>
-<script data-navigate-once src="{{ asset('adminlte3/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script data-navigate-once src="{{ asset('adminlte3/dist/js/adminlte.min.js') }}"></script> --}}
 <!-- jQuery -->
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
 
@@ -10,11 +7,18 @@
 <!-- AdminLTE -->
 <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
 
+
 {{-- sweet alert 2 --}}
 <script src="{{ asset('sweetalert2/dist/sweetalert2.all.min.js') }}"></script>
 
 <!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> --}}
+
+
+{{-- sweet alert 2 --}}
+<script src="{{ asset('sweetalert2/dist/sweetalert2.all.min.js') }}"></script>
+
+<!-- Bootstrap JS -->
 
 
 
--- resources/views/pimpinan/layouts/script.blade.php
+++ resources/views/pimpinan/layouts/script.blade.php
@@ -1,6 +1,4 @@
-{{-- <script data-navigate-once src="{{ asset('adminlte3/plugins/jquery/jquery.min.js') }}"></script>
-<script data-navigate-once src="{{ asset('adminlte3/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script data-navigate-once src="{{ asset('adminlte3/dist/js/adminlte.min.js') }}"></script> --}}
+
 
 <!-- jQuery -->
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
@@ -12,15 +10,12 @@
 <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
 
 
-
 {{-- sweet alert 2 --}}
 <script src="{{ asset('sweetalert2/dist/sweetalert2.all.min.js') }}"></script>
 
 <!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> --}}
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
