a21ff772fb07d85d32e7da17697498f59934ad9d
zakaria <zakariaalfatih83@gmail.com>
2025-11-15 21:10:22 +0700

    update project

--- resources/views/pimpinan/layouts/app.blade.php
+++ resources/views/pimpinan/layouts/app.blade.php
@@ -4,8 +4,27 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>HPM | @yield('title')</title>
+<!-- jQuery -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
 
-      @include('pimpinan.layouts.script')
+<!-- Bootstrap 4 -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+<!-- AdminLTE -->
+<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+
+
+
+
+{{-- sweet alert 2 --}}
+<script src="{{ asset('sweetalert2/dist/sweetalert2.all.min.js') }}"></script>
+
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
+      
       <!-- Chart.js -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 {{-- semua link --}}
@@ -33,7 +52,7 @@
 </div>
 
 <!-- jQuery -->
-
+    @include('pimpinan.layouts.script')
  
     @livewireScripts
 
