a998a6c8b4721fbd78ae6ffdd5d9764f5dc6fe98
zakaria <zakariaalfatih83@gmail.com>
2025-11-15 20:42:21 +0700

    update project

--- resources/views/manager/layouts/script.blade.php
+++ resources/views/manager/layouts/script.blade.php
@@ -8,17 +8,13 @@
 <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
 
 
-{{-- sweet alert 2 --}}
-<script src="{{ asset('sweetalert2/dist/sweetalert2.all.min.js') }}"></script>
-
-<!-- Bootstrap JS -->
-{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> --}}
 
 
 {{-- sweet alert 2 --}}
 <script src="{{ asset('sweetalert2/dist/sweetalert2.all.min.js') }}"></script>
 
 <!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
-
-
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
--- resources/views/pimpinan/layouts/script.blade.php
+++ resources/views/pimpinan/layouts/script.blade.php
@@ -1,5 +1,3 @@
-
-
 <!-- jQuery -->
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
 
@@ -10,8 +8,17 @@
 <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
 
 
+
+
 {{-- sweet alert 2 --}}
 <script src="{{ asset('sweetalert2/dist/sweetalert2.all.min.js') }}"></script>
+
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
+
 
 <!-- Bootstrap JS -->
 {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> --}}
