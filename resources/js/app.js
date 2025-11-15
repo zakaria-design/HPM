14aabd712ae1622531d1b39bca452bb239249875
zakaria <zakariaalfatih83@gmail.com>
2025-11-15 22:01:55 +0700

    update project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import $ from 'jquery'
+import 'bootstrap'
+import 'admin-lte'
+import Swal from 'sweetalert2'
+
+// Jika butuh akses di window:
+window.$ = $;
+window.jQuery = $;
+window.Swal = Swal;
